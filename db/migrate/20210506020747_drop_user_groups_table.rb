c45a5a8752d02526f788106567adabff8dcdfd66
Nick King <penguin@frozendesert.net>
2021-06-09 08:18:28 -0700

    Fix migration refactoring

--- db/migrate/20210506020747_drop_user_groups_table.rb
+++ db/migrate/20210506020747_drop_user_groups_table.rb
@@ -2,9 +2,6 @@
 
 class DropUserGroupsTable < ActiveRecord::Migration[6.1]
   def change
-    remove_column :agents, :group_id, type: :uuid
-    remove_column :jobs, :group_id, type: :uuid
-    remove_column :storage_backends, :group_id, type: :uuid
     drop_table :user_groups do |t|
       t.string :role
     end
